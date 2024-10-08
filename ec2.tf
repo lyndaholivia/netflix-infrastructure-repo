8b354d943aab8ab352b1d28bc9f84adca524e0a8
Lynda Nwabudike <lynda.holivia@gmail.com>
2024-10-08 01:55:32 +0100

    modified and added files

--- ec2.tf
+++ ec2.tf
@@ -1,1 +1,9 @@
 #this creates ec2 instance
+# resource "aws_instance" "web" {
+#   ami           = "ami-0c2ab3b8efb09f272" # us-east-1
+#   instance_type = "t2.micro"
+#   key_name      = "firstkey"
+#   tags = {
+#     Name = "HelloWorld"
+#   }
+# }
